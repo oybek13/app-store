1563b9057553a8bc30cb20f1e86cdc4c02ee7986
OYBEK KARIMJANOV <oybek.karimjanov13@gmail.com>
2021-12-17 00:36:43 +0500

    my first commit

---
+++ src/main/java/uz/team/entity/Category.java
@@ -0,0 +1,25 @@
+package uz.team.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @ManyToOne
+    private Category parentCategory;
+
+    private boolean active;
+}
---
+++ src/main/java/uz/team/entity/Measurement.java
@@ -0,0 +1,25 @@
+package uz.team.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Measurement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private boolean active;
+}

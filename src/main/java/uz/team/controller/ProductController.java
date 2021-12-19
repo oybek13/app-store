63da36de827e9751cf6e1ae4846a35528761a803
OYBEK KARIMJANOV <oybek.karimjanov13@gmail.com>
2021-12-20 00:17:14 +0500

    my fifth commit

--- src/main/java/uz/team/controller/ProductController.java
+++ src/main/java/uz/team/controller/ProductController.java
@@ -1,13 +1,13 @@
 package uz.team.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uz.team.dto.ProductDto;
 import uz.team.dto.ResultDto;
+import uz.team.entity.Product;
 import uz.team.service.ProductService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
@@ -16,9 +16,32 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
+    /*TODO Create*/
     @PostMapping
     public ResultDto add(@RequestBody ProductDto productDto){
-        ResultDto resultDto = productService.addProduct(productDto);
-        return resultDto;
+        return productService.addProduct(productDto);
+    }
+
+    /*TODO Get*/
+    @GetMapping
+    public List<Product> get(){
+        return productService.getAllService();
+    }
+
+    /*TODO CreateOne*/
+    @GetMapping("/{id}")
+    public Product getOneProduct(@PathVariable Integer id){
+        return productService.getOneService(id);
+    }
+
+    /*TODO Update*/
+    @PutMapping("/{id}")
+    public ResultDto update(@PathVariable Integer id, @RequestBody ProductDto productDto){
+        return productService.updateProduct(id, productDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResultDto delete(@PathVariable Integer id){
+        return productService.deleteService(id);
     }
 }
--- src/main/java/uz/team/service/ProductService.java
+++ src/main/java/uz/team/service/ProductService.java
@@ -13,6 +13,7 @@ import uz.team.entity.Category;
 import uz.team.entity.Measurement;
 import uz.team.entity.Product;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -27,6 +28,7 @@ public class ProductService {
     @Autowired
     AttachmentRepository attachmentRepository;
 
+    /*TODO CREATE*/
     public ResultDto addProduct(ProductDto productDto) {
         boolean existsByNameAndCategoryId = productRepository.existsByNameAndCategoryId(productDto.getName(), productDto.getCategoryId());
         if (existsByNameAndCategoryId)
@@ -56,5 +58,61 @@ public class ProductService {
 
         productRepository.save(product);
         return new ResultDto("Product successfully added!", true);
+    }
+
+    /*TODO GET ALL*/
+    public List<Product> getAllService(){
+        List<Product> all = productRepository.findAll();
+        return all;
+    }
+
+    /*TODO GET ONE*/
+    public Product getOneService(Integer id){
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if (optionalProduct.isPresent()){
+            Product product = optionalProduct.get();
+            return product;
+        }
+        return null;
+    }
+
+    /*TODO UPDATE*/
+    public ResultDto updateProduct(Integer id, ProductDto productDto){
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if (optionalProduct.isPresent()){
+            Product editingProduct = optionalProduct.get();
+            editingProduct.setName(productDto.getName());
+            editingProduct.setCode("1");
+
+            Optional<Category> optionalCategory = categoryRepository.findById(productDto.getCategoryId());
+            if (optionalCategory.isPresent()){
+                Category category = optionalCategory.get();
+                editingProduct.setCategory(category);
+            }
+
+            Optional<Attachment> optionalAttachment = attachmentRepository.findById(productDto.getPhotoId());
+            if (optionalAttachment.isPresent()){
+                Attachment attachment = optionalAttachment.get();
+                editingProduct.setPhoto(attachment);
+            }
+
+            Optional<Measurement> optionalMeasurement = measurementRepository.findById(productDto.getMeasurementId());
+            if (optionalMeasurement.isPresent()){
+                Measurement measurement = optionalMeasurement.get();
+                editingProduct.setMeasurement(measurement);
+            }
+
+            productRepository.save(editingProduct);
+
+            return new ResultDto("Product successfully updated!!!", true);
+
+        }
+        return new ResultDto("Not updated! Sorry :( ", false);
+    }
+
+    /*TODO DELETE*/
+    public ResultDto deleteService(Integer id){
+        productRepository.deleteById(id);
+        return new ResultDto("Product successfully deleted!", true);
     }
 }
